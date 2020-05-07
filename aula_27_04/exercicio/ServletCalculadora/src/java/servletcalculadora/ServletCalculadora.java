477e33b17ef0eb047799e186eebbc93e40af1170
jeferw <43427808+jeferw@users.noreply.github.com>
2020-05-07 18:58:16 -0300

    Servlet Calculadora

---
+++ aula_27_04/exercicio/ServletCalculadora/src/java/servletcalculadora/ServletCalculadora.java
@@ -0,0 +1,61 @@
+package servletcalculadora;
+
+import ejbconnection.EJBConnection;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/calcular")
+public class ServletCalculadora extends HttpServlet
+{
+
+    private void processarRequest(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException, IOException
+    {
+        request.setCharacterEncoding("UTF-8");
+
+        String projeto = request.getParameter("projeto");
+        String classe = request.getParameter("classe");
+        String metodo = request.getParameter("metodo");
+
+        if (projeto == null || projeto.equals(""))
+        {
+            projeto = "EBJCalculadora";
+            classe = "Calculadora";
+            metodo = "pagina";
+        }
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter saida = response.getWriter())
+        {
+
+            try
+            {
+                saida.print(EJBConnection.execMethod(projeto + "/" + classe, metodo, request));
+            } catch (Exception ex)
+            {
+                saida.print("<html>");
+                saida.print("<body>");
+                saida.print("<p><strong>Ocorreu um erro: " + ex.getMessage() + "</strong></p>");
+                saida.print("</body>");
+                saida.print("</html>");
+            }
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        processarRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        processarRequest(req, resp);
+    }
+}
